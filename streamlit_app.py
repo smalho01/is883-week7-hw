9e52ab7f294b62c60ec0e5e5ec83041a9ca88932
Sahil Malhotra <88040167+smalho01@users.noreply.github.com>
2024-11-04 14:26:58 +0000

    make input be text instead of chat

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,6 @@ chat = ChatOpenAI(openai_api_key=openai_api_key)
 st.title("Trip Review Chatbot")
 st.write("This is a simple chatbot to record and analyze your most recent trip")
 
-if prompt := st.chat_input("Share with us your experience of the latest trip:"):
+if prompt := st.text_input("Share with us your experience of the latest trip:"):
 
     print(prompt)
